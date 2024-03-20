fc7e413b5e63c020b24408ce4c86d23fd922df83
Radiy <zhambakievrr@averon.ru>
2024-03-20 15:46:23 +0500

    Writed a code for NIOS processor, need testing

---
+++ DE1_SoC_demo/sw/nios/application/DE1_SoC_demo_nios/nios.c
@@ -0,0 +1,56 @@
+/*
+ * nios.c
+ *
+ *  Created on: 20 марта 2024 г.
+ *      Author: zhambakievrr
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "io.h"
+#include "altera_avalon_pio_regs.h"
+#include "system.h"
+
+#define LEDS_MAX_ITERATION (1000)
+#define SLEEP_DELAY_US (100*1000)
+
+void rotate_leds() {
+	int loop_count = 0;
+	int leds_mask = 0x01;
+
+	// 0/1 = left/right direction
+	int led_direction = 0;
+
+	while (loop_count < LEDS_MAX_ITERATION) {
+		uint32_t switches_value = IORD_ALTERA_AVALON_PIO_DATA(SWITCHES_0_BASE);
+		uint32_t leds_value = ~leds_mask;
+
+		// only turn on leds which have their corresponding switch enabled
+		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_0_BASE, leds_value & switches_value);
+
+		usleep(SLEEP_DELAY_US);
+
+		if (led_direction == 0) {
+			leds_mask <<= 1;
+
+			if (leds_mask == (0x01 << (LEDS_0_DATA_WIDTH - 1))) {
+				led_direction = 1;
+			}
+		} else {
+			leds_mask >>= 1;
+
+			if (leds_mask == 0x01) {
+				led_direction = 0;
+				loop_count++;
+			}
+		}
+	}
+}
+
+int main() {
+	rotate_leds();
+	return 0;
+}
+
+
---
+++ DE1_SoC_demo/sw/nios/application/DE1_SoC_demo_nios/obj/default/nios.d
@@ -0,0 +1,16 @@
+obj/default/nios.o: nios.c \
+ C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp//HAL/inc/io.h \
+ C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp//HAL/inc/alt_types.h \
+ C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp/system.h \
+ C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp/linker.h
+
+C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp//HAL/inc/io.h:
+
+C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp//HAL/inc/alt_types.h:
+
+C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp/system.h:
+
+C:/repos/quartus_proj/BM-CycloneV-DE1_SoC/DE1_SoC_demo/sw/nios/application/DE1-SoC_demo_nios_bsp/linker.h:
